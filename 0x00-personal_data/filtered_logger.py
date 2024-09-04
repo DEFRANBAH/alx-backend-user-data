6075bb71af30b0f54c1c0f208932233060050df6
kelvin defrNBAH <kevinocholah@gmail.com>
2024-09-05 01:08:13 +0300

    add filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,3 +21,23 @@ def filter_datum(fields, redaction, message, separator):
     pattern = f"({'|'.join(fields)})=([^\\{separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
 
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ 
+    Custom Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        # Use filter_datum to filter out sensitive fields in the log message
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
